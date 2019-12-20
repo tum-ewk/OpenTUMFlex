326141f9e65c3d33950150b05805796b5df11759
Michel Zadé <michel.zade@tum.de>
2019-12-20 17:43:18 +0100

    Integration of flex prices

--- ems/flex/flex_ev.py
+++ ems/flex/flex_ev.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 import statistics
 from heapq import nsmallest
+from heapq import nlargest
 
 
 def calc_flex_ev(my_ems):
@@ -17,12 +18,13 @@ def calc_flex_ev(my_ems):
 
     # Flexibility Table for entire time period #############################################
     ev_flex = pd.DataFrame(0, columns={'Opt_Power', 'Pos_Flex_Power', 'Neg_Flex_Power', 'Pos_Flex_Energy',
-                                       'Neg_Flex_Energy', 'Pos_Flex_Price', 'Neg_Flex_Price'},
+                                       'Neg_Flex_Energy', 'Pos_Flex_Price', 'Neg_Flex_Price', 'Price_Fcst'},
                            index=pd.date_range(start=my_ems['time_data']['start_time'],
                                                end=my_ems['time_data']['end_time'],
                                                freq=str(my_ems['time_data']['t_inval']) + 'Min'))
 
     ev_flex.Opt_Power.at[:] = my_ems['optplan']['EV_power']
+    ev_flex.Price_Fcst.at[:] = my_ems['fcst']['ele_price_in']
 
     # Check number of periods ev is available
     n_avail_periods = len(my_ems['devices']['ev']['initSOC'])
@@ -31,13 +33,14 @@ def calc_flex_ev(my_ems):
     for j in range(n_avail_periods):
         ev_flex_temp = pd.DataFrame(0.0, columns={'Opt_Power', 'Remaining_Energy', 'Pos_Flex_Power', 'Neg_Flex_Power',
                                                   'Pos_Flex_Energy', 'Neg_Flex_Energy', 'Pos_Cum_Flex_Energy',
-                                                  'Neg_Cum_Flex_Energy', 'Pos_Flex_Price', 'Neg_Flex_Price'},
+                                                  'Neg_Cum_Flex_Energy', 'Pos_Flex_Price', 'Neg_Flex_Price', 'Price_Fcst'},
                                     index=pd.date_range(start=my_ems['devices']['ev']['aval_init'][j],
                                                         end=my_ems['devices']['ev']['aval_end'][j],
                                                         freq=str(my_ems['time_data']['t_inval'])+'Min'))
 
         # Copy optimal power to flex table
         ev_flex_temp.Opt_Power.at[:] = ev_flex.Opt_Power[my_ems['devices']['ev']['aval_init'][j]:my_ems['devices']['ev']['aval_end'][j]]
+        ev_flex_temp.Price_Fcst.at[:] = ev_flex.Price_Fcst[my_ems['devices']['ev']['aval_init'][j]:my_ems['devices']['ev']['aval_end'][j]]
 
         # Calculate remaining energy that needs to be charged in kWh ####
         # Initially remaining energy is equal to desired end soc minus start soc multiplied by capacity
@@ -108,10 +111,10 @@ def calc_flex_ev(my_ems):
                 ev_flex_temp.Pos_Flex_Power.iat[i] = 0
 
         # Get all indices for negative and positive flex offers
-        neg_flex_indices = [i for i, x in enumerate(ev_flex_temp.Neg_Cum_Flex_Energy) if x]
+        neg_flex_idx = [i for i, x in enumerate(ev_flex_temp.Neg_Cum_Flex_Energy) if x]
         pos_flex_indices = [i for i, x in enumerate(ev_flex_temp.Pos_Cum_Flex_Energy) if x]
         # check whether offered negative energy is above remaining energy amount
-        for i in neg_flex_indices:
+        for i in neg_flex_idx:
             if ev_flex_temp.Neg_Cum_Flex_Energy.iat[i] > ev_flex_temp.Remaining_Energy.iat[i]:
                 ev_flex_temp.Neg_Cum_Flex_Energy.iat[i] = ev_flex_temp.Remaining_Energy.iat[i]
 
@@ -124,15 +127,23 @@ def calc_flex_ev(my_ems):
         for i in range(len(ev_flex_temp)):
             # Positive flexibility
             if ev_flex_temp.Pos_Flex_Energy.iat[i] > 0:
+                # Calculate how many time steps energy needs to be charged after flex call
+                n_charge_remainder_tsteps = math.ceil(ev_flex_temp.Remaining_Energy.iat[i] / \
+                                    my_ems['devices']['ev']['maxpow'] * n_time_steps_phour)
                 n_flex_delivery_tsteps = math.ceil(ev_flex_temp.Pos_Flex_Energy.iat[i] / \
                                                    ev_flex_temp.Pos_Flex_Power.iat[i] * n_time_steps_phour)
+                ev_flex_temp.Pos_Flex_Price.iat[i] = statistics.mean(nsmallest
+                                                                     (n_charge_remainder_tsteps,
+                                                                      ev_flex_temp.Price_Fcst[i + n_flex_delivery_tsteps:])) * (1 + risk_margin)
 
-
-                # Takes mean of n smallest forecast electricity prices from end of flex till end of availability
-                #ev_flex_temp.Pos_Flex_Price = statistics.mean(nsmallest(n_flex_delivery_tsteps, my_ems['fcst']['ele_price_in'][i+n_flex_delivery_tsteps:]) * (1 + risk_margin))
             # Negative flexibility
-            #elif ev_flex_temp.Neg_Flex_Energy.iat[i] > 0:
+            elif ev_flex_temp.Neg_Flex_Energy.iat[i] > 0:
+                n_flex_delivery_tsteps = math.ceil(ev_flex_temp.Neg_Flex_Energy.iat[i] / \
+                                                   ev_flex_temp.Neg_Flex_Power.iat[i] * n_time_steps_phour)
+                ev_flex_temp.Neg_Flex_Price.iat[i] = statistics.mean(nlargest(n_flex_delivery_tsteps,
+                                                                              ev_flex_temp.Price_Fcst[i + n_flex_delivery_tsteps:])) * (risk_margin - 1)
 
+            print(i)
 
 
         # Clean-up ##########################################
@@ -140,5 +151,6 @@ def calc_flex_ev(my_ems):
         ev_flex_temp = ev_flex_temp.drop(columns={'Remaining_Energy', 'Pos_Cum_Flex_Energy', 'Neg_Cum_Flex_Energy'})
 
         # Copy temporary data frame to overall dataframe
+        ev_flex[my_ems['devices']['ev']['aval_init'][j]:my_ems['devices']['ev']['aval_end'][j]] = ev_flex_temp
 
     return ev_flex
--- tests/test.py
+++ tests/test.py
@@ -33,13 +33,10 @@ my_ems = ems_loc(initialize=True, path='data/test_Nr_01.txt')
 # change the time interval
 my_ems['time_data']['t_inval'] = 15
 my_ems['time_data']['d_inval'] = 15
-<<<<<<< HEAD
 my_ems['time_data']['start_time'] = '2019-12-18 00:00'
 my_ems['time_data']['end_time'] = '2019-12-18 23:45'
-=======
 my_ems['time_data']['start_time'] = '2019-12-18 02:00'
 my_ems['time_data']['end_time'] = '2019-12-19 11:59'
->>>>>>> a670e5041d287a4e34a3aeeaf449b99f2bd660d7
 my_ems['time_data']['days'] = 1
 my_ems.update(update_time_data(my_ems))
 
@@ -57,10 +54,10 @@ my_ems['devices']['pv']['maxpow'] = 5
 my_ems['devices']['bat']['stocap'] = 10
 my_ems['devices']['bat']['maxpow'] = 10
 my_ems['devices'].update(devices(device_name='ev', minpow=0, maxpow=8, stocap=40, init_soc=[20, 35, 30],
-                                 end_soc=[50, 50, 20], eta=0.98,
+                                 end_soc=[50, 50, 50], eta=0.98,
                                  ev_aval=["2019-12-18 4:00", "2019-12-18 9:00",
                                           "2019-12-18 13:45", "2019-12-18 18:15",
-                                          "2019-12-19 9:30", "2019-12-19 11:15"],
+                                          "2019-12-18 20:30", "2019-12-18 23:15"],
                                  timesetting=my_ems['time_data']))
 
 
