888f252c03e03ff7b11725d673e0b31deb5d4bac
Michel Zadé <michel.zade@tum.de>
2020-03-30 20:46:53 +0200

    Computation of ev flexibility with rtp

--- tests/chts_ev_flex_computation.py
+++ tests/chts_ev_flex_computation.py
@@ -15,11 +15,12 @@ from ems.optim.optimize_EV_charging import create_ev_model
 
 from pyomo.opt import SolverFactory
 # import flex devices modules
-from ems.flex.flex_ev import  calc_flex_ev
+from ems.flex.flex_ev import calc_flex_ev
 from ems.plot.flex_draw import plot_flex as plot
 
 # Read home availabilities from file
 veh_availability = pd.read_csv('data/chts_veh_availability.csv')
+rtp_price_forecast = pd.read_hdf('C:/Users/ga47num/PycharmProjects/CHTS - OpenTUMFlex - EV/Input-Data/RTP/rtp_15min_201802010000-201903010000.h5', key='df')
 
 # Total number of home availabilities with trips conducted before and afterwards
 n_avail = len(veh_availability)
@@ -39,13 +40,17 @@ t_insufficient_count = 0
 results = list()
 
 # Go through all vehicle availabilities
+# for i in range(0, 10):
 for i in range(len(veh_availability)):
     # Ceil arrival time to next quarter hour
     t_arrival_ceiled = pd.Timestamp(veh_availability['t_arrival'][i]).ceil(freq='15Min')
     # Floor departure time to previous quarter hour
     t_departure_floored = pd.Timestamp(veh_availability['t_departure'][i]).floor(freq='15Min')
     # Check whether time between ceiled arrival and floored departure time are at least two time steps
-    # t_delta_ts = t_departure_floored - t_arrival_ceiled
+    if t_arrival_ceiled >= t_departure_floored:
+        print('### Time is not sufficient for timestep:', i, '###')
+        t_insufficient_count += 1
+        continue
 
     # change the time interval
     my_ems['time_data']['t_inval'] = 15
@@ -56,15 +61,11 @@ for i in range(len(veh_availability)):
     my_ems.update(update_time_data(my_ems))
 
     # Get price forecast for given time period
-    price_fcst = get_elect_price_fcst(t_start=t_arrival_ceiled, t_end=t_departure_floored,
-                                      min_price_increment=False, pr_constant=0.19)
-    # Check whether price forecast is empty
-    if price_fcst is None:
-        print('### Time is not sufficient for timestep:', i, '###')
-        t_insufficient_count += 1
-        continue
+    price_fcst = get_elect_price_fcst(t_start=t_arrival_ceiled, t_end=t_departure_floored, pr_constant=0.19)
+    price_fcst.insert(value=rtp_price_forecast['price'].loc[t_arrival_ceiled:t_departure_floored], loc=0, column='RTP')
+
     # Update forecast data
-    my_ems['fcst']['ele_price_in'] = price_fcst['ToU'].to_list()
+    my_ems['fcst']['ele_price_in'] = price_fcst['RTP'].to_list()
     my_ems['fcst']['ele_price_out'] = len(price_fcst) * [0]
     my_ems['fcst']['gas'] = len(price_fcst) * [0]
     my_ems['fcst']['last_elec'] = len(price_fcst) * [0]
@@ -89,5 +90,5 @@ for i in range(len(veh_availability)):
     # plot(my_ems, 'ev')
 
     # Save results to files
-    results.append(my_ems)
-    ems_write(my_ems, path='C:/Users/ga47num/PycharmProjects/CHTS - OpenTUMFlex - EV - Results/ToU/ev_avail_' + str(i) + '.txt')
+    #results.append(my_ems)
+    ems_write(my_ems, path='C:/Users/ga47num/PycharmProjects/CHTS - OpenTUMFlex - EV/Results/RTP/ev_avail_' + str(i) + '.txt')
