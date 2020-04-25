400a5cb0bb42430128098cc353283a675a0b4555
Michel Zadé <michel.zade@tum.de>
2020-04-25 12:38:02 +0200

    Adaptation to different price schemes

--- tests/ev_flex_computation.py
+++ tests/ev_flex_computation.py
@@ -18,9 +18,9 @@ from pyomo.opt import SolverFactory
 from ems.flex.flex_ev import calc_flex_ev
 from ems.plot.flex_draw import plot_flex as plot
 
-# Read home availabilities from file
-veh_availability = pd.read_csv('data/chts_veh_availability.csv')
-rtp_price_forecast = pd.read_hdf('C:/Users/ga47num/PycharmProjects/CHTS - OpenTUMFlex - EV/Input-Data/RTP/rtp_15min_201802010000-201903010000.h5', key='df')
+# Read home availabilities and real time prices from file
+veh_availability = pd.read_csv('C:/Users/ga47num/PycharmProjects/GER MP - OpenTUMFlex - EV/Input/Veh_Availability/ger_mp_veh_availability.csv')
+rtp_price_forecast = pd.read_hdf('C:/Users/ga47num/PycharmProjects/GER MP - OpenTUMFlex - EV/Input/RTP/rtp_15min_201701010000-201801010000.h5', key='df')
 
 # Total number of home availabilities with trips conducted before and afterwards
 n_avail = len(veh_availability)
@@ -30,7 +30,6 @@ mil2km_conversion = 1.61            # Miles to kilometer conversion rate
 electr_consumption_per_km = 0.2     # electricity consumption per km (e.g. 0.2 equals 20kWh/100km
 
 # load the predefined ems data, initialization by user input is also possible:
-my_ems = ems_loc(initialize=True, path='data/test_Nr_01.txt')
 my_ems = ems_loc(initialize=True, path='data/ev_ems_sa_constant_price_incl_error.txt')
 
 # Counter for keeping track of insufficient time differences
@@ -40,7 +39,7 @@ t_insufficient_count = 0
 results = list()
 
 # Go through all vehicle availabilities
-# for i in range(0, 10):
+# for i in range(0, 3):
 for i in range(len(veh_availability)):
     # Ceil arrival time to next quarter hour
     t_arrival_ceiled = pd.Timestamp(veh_availability['t_arrival'][i]).ceil(freq='15Min')
@@ -63,32 +62,36 @@ for i in range(len(veh_availability)):
     # Get price forecast for given time period
     price_fcst = get_elect_price_fcst(t_start=t_arrival_ceiled, t_end=t_departure_floored, pr_constant=0.19)
     price_fcst.insert(value=rtp_price_forecast['price'].loc[t_arrival_ceiled:t_departure_floored], loc=0, column='RTP')
+    price_fcst = price_fcst.drop(columns={'Random', 'EPEX'})
 
-    # Update forecast data
-    my_ems['fcst']['ele_price_in'] = price_fcst['RTP'].to_list()
-    my_ems['fcst']['ele_price_out'] = len(price_fcst) * [0]
-    my_ems['fcst']['gas'] = len(price_fcst) * [0]
-    my_ems['fcst']['last_elec'] = len(price_fcst) * [0]
-    my_ems['fcst']['last_heat'] = len(price_fcst) * [0]
-    my_ems['fcst']['temp'] = len(price_fcst) * [0]
-    my_ems['fcst']['solar'] = len(price_fcst) * [0]
+    for price in price_fcst.columns:
+        print(price)
+        # Update forecast data
+        my_ems['fcst']['ele_price_in'] = price_fcst[price].to_list()
+        my_ems['fcst']['ele_price_out'] = len(price_fcst) * [0]
+        my_ems['fcst']['gas'] = len(price_fcst) * [0]
+        my_ems['fcst']['last_elec'] = len(price_fcst) * [0]
+        my_ems['fcst']['last_heat'] = len(price_fcst) * [0]
+        my_ems['fcst']['temp'] = len(price_fcst) * [0]
+        my_ems['fcst']['solar'] = len(price_fcst) * [0]
 
-    # Update EV parameters
-    my_ems['devices'].update(devices(device_name='ev', minpow=0, maxpow=11,
-                                     stocap=round(veh_availability['d_travelled'][i]*mil2km_conversion*electr_consumption_per_km),
-                                     init_soc=[0], end_soc=[100], eta=0.98,
-                                     ev_aval=[my_ems['time_data']['start_time'], my_ems['time_data']['end_time']],
-                                     timesetting=my_ems['time_data']))
+        # Update EV parameters
+        my_ems['devices'].update(devices(device_name='ev', minpow=0, maxpow=11,
+                                         stocap=round(veh_availability['d_travelled'][i]*electr_consumption_per_km),
+                                         init_soc=[0], end_soc=[100], eta=0.98,
+                                         ev_aval=[my_ems['time_data']['start_time'], my_ems['time_data']['end_time']],
+                                         timesetting=my_ems['time_data']))
 
-    # calculate the timetable for all the devices
-    my_ems['optplan'] = opt(my_ems, plot_fig=False, result_folder='data/')
+        # calculate the timetable for all the devices
+        my_ems['optplan'] = opt(my_ems, plot_fig=False, result_folder='data/')
 
-    # Calculate ev flexibility
-    my_ems['flexopts']['ev'] = calc_flex_ev(my_ems)
+        # Calculate ev flexibility
+        my_ems['flexopts']['ev'] = calc_flex_ev(my_ems)
 
-    # Plot flex result
-    # plot(my_ems, 'ev')
+        # Plot flex result
+        # plot(my_ems, 'ev')
 
-    # Save results to files
-    #results.append(my_ems)
-    ems_write(my_ems, path='C:/Users/ga47num/PycharmProjects/CHTS - OpenTUMFlex - EV/Results/RTP/ev_avail_' + str(i) + '.txt')
+        # Save results to files
+        # results.append(my_ems)
+        ems_write(my_ems, path='C:/Users/ga47num/PycharmProjects/GER MP - OpenTUMFlex - EV/Output/' + price +
+                               '/ev_avail_' + str(i) + '.txt')
