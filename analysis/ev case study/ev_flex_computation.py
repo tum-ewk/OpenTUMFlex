666964616c70359ad790d15e296d3c01a7a0c0e4
Michel Zadé <michel.zade@tum.de>
2020-09-17 17:20:35 +0200

    Adaptations to the new master branch

--- analysis/ev case study/ev_flex_computation.py
+++ analysis/ev case study/ev_flex_computation.py
@@ -1,22 +1,27 @@
-import numpy as np
-import pandas as pd
+"""
+The ev_flex_computation module calculates the flexibility of a list of vehicle availabilities .
+"""
 
+__author__ = "Michel Zadé"
+__copyright__ = "Copyright 2020, OpenTUMFlex.py"
+__credits__ = []
+__license__ = ""
+__version__ = "1.0"
+__maintainer__ = "Michel Zadé"
+__email__ = "michel.zade@tum.de"
+__status__ = "Development"
+
+import pandas as pd
 from ems.ems_mod import ems as ems_loc
 from ems.ems_mod import ems_write
 from ems.ems_mod import update_time_data
 from ems.devices.devices import devices
-
-from forecast.fcst import load_data
 from forecast.price_fcst import get_elect_price_fcst
-
 from ems.optim.opt_test import run_hp_opt as opt
-from ems.optim.optimize_EV_charging import create_ev_model
-
-from pyomo.opt import SolverFactory
-# import flex devices modules
 from ems.flex.flex_ev import calc_flex_ev
-from ems.plot.flex_draw import plot_flex as plot
 import os
+from ems.ems_mod import initialize_devices
+
 
 p_charge_max = 3.7                   # Maximal charging power
 mil2km_conversion = 1.61            # Miles to kilometer conversion rate
@@ -39,6 +44,18 @@ n_veh = len(veh_availability['vehID'].unique())
 
 # load the predefined ems data, initialization by user input is also possible:
 my_ems = ems_loc(initialize=True, path='data/ev_ems_sa_constant_price_incl_error.txt')
+# Initialize ems settings
+# Time settings
+my_ems = {'time_data': {}}
+my_ems['time_data']['t_inval'] = 15
+my_ems['time_data']['d_inval'] = 15
+my_ems['time_data']['start_time'] = '2017-12-18 09:30'
+my_ems['time_data']['end_time'] = '2017-12-18 17:00'
+my_ems['time_data']['days'] = 1
+my_ems.update(update_time_data(my_ems))
+# Initialize household devices
+initialize_devices(my_ems)
+my_ems['fcst'] = {}
 
 # Counter for keeping track of insufficient time differences
 t_insufficient_count = 0
@@ -61,12 +78,15 @@ for i in range(len(veh_availability)):
         continue
 
     # change the time interval
-    my_ems['time_data']['t_inval'] = 15
-    my_ems['time_data']['d_inval'] = 15
     my_ems['time_data']['start_time'] = t_arrival_ceiled.strftime('%Y-%m-%d %H:%M')
     my_ems['time_data']['end_time'] = t_departure_floored.strftime('%Y-%m-%d %H:%M')
-    my_ems['time_data']['days'] = 1
     my_ems.update(update_time_data(my_ems))
+    my_ems['fcst']['temp'] = [0] * my_ems['time_data']['nsteps']
+    my_ems['fcst']['solar'] = [0] * my_ems['time_data']['nsteps']
+    my_ems['fcst']['last_heat'] = [0] * my_ems['time_data']['nsteps']
+    my_ems['fcst']['last_elec'] = [0] * my_ems['time_data']['nsteps']
+    my_ems['fcst']['gas'] = [0] * my_ems['time_data']['nsteps']
+    my_ems['fcst']['ele_price_out'] = [0] * my_ems['time_data']['nsteps']
 
     # Get price forecast for given time period
     price_fcst = get_elect_price_fcst(t_start=t_arrival_ceiled, t_end=t_departure_floored, pr_constant=0.19)
@@ -77,12 +97,6 @@ for i in range(len(veh_availability)):
         print(price)
         # Update forecast data
         my_ems['fcst']['ele_price_in'] = price_fcst[price].to_list()
-        my_ems['fcst']['ele_price_out'] = len(price_fcst) * [0]
-        my_ems['fcst']['gas'] = len(price_fcst) * [0]
-        my_ems['fcst']['last_elec'] = len(price_fcst) * [0]
-        my_ems['fcst']['last_heat'] = len(price_fcst) * [0]
-        my_ems['fcst']['temp'] = len(price_fcst) * [0]
-        my_ems['fcst']['solar'] = len(price_fcst) * [0]
 
         # Update EV parameters
         my_ems['devices'].update(devices(device_name='ev', minpow=0, maxpow=p_charge_max,
@@ -97,9 +111,9 @@ for i in range(len(veh_availability)):
         # Calculate ev flexibility
         my_ems['flexopts']['ev'] = calc_flex_ev(my_ems)
 
-        # Plot flex result
+        # # Plot flex result
         # plot(my_ems, 'ev')
 
-        # Save results to files
+        # # Save results to files
         # results.append(my_ems)
-        ems_write(my_ems, path=output_path + price + '/ev_avail_' + str(i) + '.txt')
+        # ems_write(my_ems, path=output_path + price + '/ev_avail_' + str(i) + '.txt')
