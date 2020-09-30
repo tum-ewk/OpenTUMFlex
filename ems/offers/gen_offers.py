db2eba468b0209b9528975349b598d9b0d30e19b
Babu Kumaran Nalini <knbamritian@gmail.com>
2020-09-30 15:56:43 +0200

    Alf markt flex offer generator.

--- ems/offers/gen_offers.py
+++ ems/offers/gen_offers.py
@@ -39,49 +39,28 @@ def alf_markt(my_ems,device):
                                'Energie+','Preis-','Preis+', 'Teilabruf', 
                                'max_Anzahl_Abrufe_pro_Tag', 'max_Dauer_pro_Tag_in_h',
                                'Dauer_zwischen_zwei_Abrufen_in_h', 'max_Dauer_eines_Abrufs',
-                               'min_Dauer_eines_Abrufs'], index=range(1,96))
+                               'min_Dauer_eines_Abrufs'], index=range(my_ems['time_data']['nsteps']))
+                       
+    df['Leistung_Plan'] = round(my_ems['flexopts'][device]['Sch_P'], 5)
+    df['Leistung-'] = round(my_ems['flexopts'][device]['Neg_P'], 5)
+    df['Leistung+'] = round(my_ems['flexopts'][device]['Pos_P'], 5)
+    df['Energie-'] = round(my_ems['flexopts'][device]['Neg_E'], 5)
+    df['Energie+'] = round(my_ems['flexopts'][device]['Pos_E'], 5)
+    df['Preis-'] = round(my_ems['flexopts'][device]['Neg_Pr']*100, 2)
+    df['Preis+'] = round(my_ems['flexopts'][device]['Pos_Pr']*100, 2)
     
     # Add UTC string to Uhrzeit
-    UTC = " +0100"
-    
-    for i in range(0,95):
-        for j in range(0,8):
-            if j != 0:
-                if j==6 or j==7:
-                    # Converting to cents
-                    df.iloc[i][j] = round(my_ems['flexopts'][device].iloc[i][j]*100, 2)                     
-                else:
-                    df.iloc[i][j] = round(my_ems['flexopts'][device].iloc[i][j], 5)
-            else:
-                temp = my_ems['flexopts'][device].iloc[i][j]
-                if isinstance(temp, str):
-                    datetime_object = datetime.strptime(temp, "%Y-%m-%d %H:%M")
-                    df.iloc[i][j] = datetime_object.strftime("%d.%m.%Y %H:%M") + UTC
-                else:
-                    print("check whether flex offer time column is available?")
+    UTC = " +0100"  
+    df['Uhrzeit'] = my_ems['time_data']['time_slots'] + UTC
      
     # Additional parameters           
-    df.iloc[0,8] = "Ja"  #Teilberuf
-    df.iloc[0,9] = 2     #max_Anzahl_Abrufe_pro_Tag
-    df.iloc[0,10] = 2.25     #max_Dauer_pro_Tag_in_h
-    df.iloc[0,11] = 1.5     #Dauer_zwischen_zwei_Abrufen_in_h
-    df.iloc[0,12] = 1.5     #max_Dauer_eines_Abrufs
-    df.iloc[0,13] = 0.25     #min_Dauer_eines_Abrufs
-    
-    df['Leistung_Plan'] = df['Leistung_Plan'].astype(float)
-    df['Leistung-'] = df['Leistung-'].astype(float)
-    df['Leistung+'] = df['Leistung+'].astype(float)
-    df['Energie-'] = df['Energie-'].astype(float)
-    df['Energie+'] = df['Energie+'].astype(float)
-    df['Preis-'] = df['Preis-'].astype(float)
-    df['Preis+'] = df['Preis+'].astype(float)
-    # df['Teilabruf'] = df['Teilabruf'].astype(str)
-    df['max_Anzahl_Abrufe_pro_Tag'] = df['max_Anzahl_Abrufe_pro_Tag'].astype(float)
-    df['max_Dauer_pro_Tag_in_h'] = df['max_Dauer_pro_Tag_in_h'].astype(float)
-    df['Dauer_zwischen_zwei_Abrufen_in_h'] = df['Dauer_zwischen_zwei_Abrufen_in_h'].astype(float)
-    df['max_Dauer_eines_Abrufs'] = df['max_Dauer_eines_Abrufs'].astype(float)
-    df['min_Dauer_eines_Abrufs'] = df['min_Dauer_eines_Abrufs'].astype(float)
-    
+    df.loc[0,'Teilabruf'] = "Ja" 
+    df.loc[0,'max_Anzahl_Abrufe_pro_Tag'] = 2.0
+    df.loc[0,'max_Dauer_pro_Tag_in_h'] = 2.25
+    df.loc[0,'Dauer_zwischen_zwei_Abrufen_in_h'] = 1.5
+    df.loc[0,'max_Dauer_eines_Abrufs'] = 1.5
+    df.loc[0,'min_Dauer_eines_Abrufs'] = 0.25
+       
     # Save as CSV file
     cwd = os.getcwd()
     # back_cwd = os.path.dirname(cwd)
@@ -89,11 +68,12 @@ def alf_markt(my_ems,device):
     path = os.path.join(cwd,mdir)
     if not os.path.exists(path):
         os.mkdir(path)
+    datetime_object = datetime.strptime(my_ems['time_data']['time_slots'][0], "%Y-%m-%d %H:%M")
     file_name = datetime_object.strftime("%Y-%m-%d")+ '_' + \
         device+'_Flex-Angebot.csv'
     new_cwd = os.path.join(path,file_name)
-    df.to_csv(new_cwd, sep=';', decimal=',', index=False)
-    print("CSV file generated! Available on " + new_cwd)
+    df.to_csv(new_cwd, sep=';', index=False)
+    print("CSV file generated - flex offers for alf markt! Available on " + new_cwd)
     
     return df
     
