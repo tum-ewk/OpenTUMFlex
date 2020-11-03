4a393001d3e4de7064e38b15629eaead32f94471
Michel Zadé <michel.zade@tum.de>
2020-11-03 15:25:59 +0100

    Removal of calc_overall_costs since it has been integrated in the aggregation.

--- analysis/ev_case_study/__init__.py
+++ analysis/ev_case_study/__init__.py
@@ -2,7 +2,6 @@
 This package includes all necessary modules to perform the ev case study.
 """
 from .calc_ev_flex_offers import calc_ev_flex_offers, calc_ev_flex_offers_parallel
-from .calc_overall_cost import calc_overall_cost
 from .aggregate_ev_opt_flex import aggregate_ev_flex
 from .plot_timeseries_results import plot_opt_flex_timeseries, plot_n_avail_veh
 from .plot_flex_heatmap import plot_flex_heatmap
