a1723172d7809409f2cee7a6127a8f5fa4db93c7
Helena Hahne <23360240+helenahahne@users.noreply.github.com>
2021-04-22 10:35:00 +0200

    plot flex prices - code cleanup, comments update, deletion of old files

--- analysis/ev_case_study/plot_flex_prices.py
+++ analysis/ev_case_study/plot_flex_prices.py
@@ -7,10 +7,10 @@ __author__ = "Helena Hahne"
 __copyright__ = "2021 TUM-EWK"
 __credits__ = []
 __license__ = "GPL v3.0"
-__version__ = "0"
-__maintainer__ = "?"
-__email__ = "?"
-__status__ = "Development"
+__version__ = "1"
+__maintainer__ = ""
+__email__ = "helena.hahne@tum.de"
+__status__ = "Complete"
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -34,10 +34,9 @@ def plot_flex_prices(power, output_path, save_figure=True, figure_path='figures/
     :return:
     """
 
-    # Read aggregated all seasons data from hdf files #########################################
-    flex_per_daytime = pd.read_hdf(output_path + '/Aggregated Data/flex_per_daytime_data.h5', key='df')
+    # Read aggregated hdf files
 
-    # allseason files
+    # All seasons files
     day_flex_per_daytime = pd.read_hdf(output_path + 'Aggregated Data/allseasons_day_flex_per_daytime_data.h5', key='df')
     weekday_flex_per_daytime = pd.read_hdf(output_path + 'Aggregated Data/allseasons_weekday_flex_per_daytime_data.h5', key='df')
     weekend_opt_per_daytime = pd.read_hdf(output_path + 'Aggregated Data/allseasons_weekend_opt_per_daytime_data.h5', key='df')
@@ -54,14 +53,9 @@ def plot_flex_prices(power, output_path, save_figure=True, figure_path='figures/
     all_title_l = ['Day', 'Weekday', 'Weekend']
 
     # Check whether winter/summer files exist (if one exists, all exist) and read data from hdf files
-    # seasons = ['winter', 'summer']
-    # for season in seasons: # found no solution for not being able to name variables in loop (variable variables)
     winter_path = Path(output_path + 'Aggregated Data/winter_day_flex_per_daytime_data.h5')
     summer_path = Path(output_path + 'Aggregated Data/summer_day_flex_per_daytime_data.h5')
-    opt_dfs = []
-    flex_dfs = []
-    agg_type_l = []
-    title_l = []
+
     if winter_path.is_file():
         wi_weekday_flex_per_daytime = pd.read_hdf(output_path + 'Aggregated Data/winter_weekday_flex_per_daytime_data.h5', key='df')
         wi_weekend_opt_per_daytime = pd.read_hdf(output_path + 'Aggregated Data/winter_weekend_opt_per_daytime_data.h5', key='df')
@@ -102,37 +96,35 @@ def plot_flex_prices(power, output_path, save_figure=True, figure_path='figures/
         su_price_dfs = [su_day_flex_prices, su_weekday_flex_prices, su_weekend_flex_prices]
         su_agg_type_l = ['summer_day', 'summer_weekday', 'summer_weekend']
         su_title_l = ['Day in Summer', 'Weekday in Summer', 'Weekend Day in Summer']
+
     else:
         su_opt_dfs = []
         su_flex_dfs = []
         su_price_dfs = []
-        # su_seasons_list = []
         su_agg_type_l = []
         su_title_l = []
 
-    # Plot preparations
-    tick_range = np.linspace(start=0, stop=96, num=96)
     # Figure settings
+    tick_range = np.linspace(start=0, stop=96, num=96)
     rcParams["font.family"] = "Times New Roman"
     rcParams["mathtext.default"] = "regular"
     font_size = rcParams["font.size"] = 10
     rcParams["figure.figsize"] = [15, 11]
     plot_color = 'tab:blue'
-
-    # define number of subplots
+    # Define number of subplots
     nrows = 3
     ncols = 5
-    # percentage from highest (max & min) y value that is added to it to get the y-lim when ylims is not None
+    # Percentage from highest (abs) y value that is added to it, to get the y-limit when parameter ylims is not None
     ylim_spacing = 0.1
 
-    # lists for outer for loop (day, weekday, weekend for summer & winter each, depending on whether su and/or wi exist)
+    # Lists for outer for loop (day, weekday, weekend for summer & winter each, depending on whether su and/or wi exist)
     opt_dfs = wi_opt_dfs + su_opt_dfs + all_opt_dfs
     flex_dfs = wi_flex_dfs + su_flex_dfs + all_flex_dfs
     price_dfs = wi_price_dfs + su_price_dfs + all_price_dfs
     agg_type_l = wi_agg_type_l + su_agg_type_l + all_agg_type_l
     title_l = wi_title_l + su_title_l + all_title_l
 
-    # dicts for inner for loops (subplots, what column of dataframe is called)
+    # Dictionaries for inner for loops (subplots, what column of dataframe is called)
     fc_plot_dict = {0: {'price_tariff': 'Con', 'fc_kwh': 'c_con_kwh'},
                     1: {'price_tariff': 'Con + MI', 'fc_kwh': 'c_con_mi_kwh'},
                     2: {'price_tariff': 'ToU', 'fc_kwh': 'c_tou_kwh'},
@@ -154,23 +146,21 @@ def plot_flex_prices(power, output_path, save_figure=True, figure_path='figures/
                         4: {'max_pr_pos': 'max_c_flex_pos_rtp', 'max_pr_neg': 'max_c_flex_neg_rtp',
                             'min_pr_pos': 'min_c_flex_pos_rtp', 'min_pr_neg': 'min_c_flex_neg_rtp'}}
 
-    # for flex_per_daytime_df, opt_per_daytime_df, plottype, title in zip(flex_dfs, opt_dfs, plottype_l, title_l):
     for flex_per_daytime_df, opt_per_daytime_df, price_df, agg_type, title in zip(flex_dfs, opt_dfs, price_dfs, agg_type_l, title_l):
 
-        # create figure with nrows * ncols subplots
+        # Create figure with nrows * ncols subplots
         fig1, axs = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey='row')
         fig1.suptitle('Price Forecast, Flexible and Scheduled Power and Flexibility Prices for an Average ' +
                       str(title) + ' at ' + str(power) + ' kW', fontsize=16, y=0.98)
 
         # Price Forecast Subplots
-        # for i, pricefcast, tariff in zip(*forecast_lists):
         for i, value in fc_plot_dict.items():
             axs[0, i].plot(tick_range, opt_per_daytime_df[value['fc_kwh']], color=plot_color,
                            linestyle='solid', label='Forecast price\nmean')
             axs[0, i].grid()
-            # set subplot columns title
+            # Set subplot columns title
             axs[0, i].set_title(value['price_tariff'], fontsize=font_size)
-            # plot legend only for last subplot in row, to the right of the row
+            # Plot legend only for last subplot in row, to the right of the row
             if i == ncols - 1:
                 axs[0, i].legend(bbox_to_anchor=(1.03, .6), loc='upper left', frameon=False)
 
@@ -229,6 +219,8 @@ def plot_flex_prices(power, output_path, save_figure=True, figure_path='figures/
         tick_labels = pd.date_range(start='2020-01-01 00:00', end='2020-01-02 00:00', freq='15Min').strftime(
             '%H:%M').to_list()
         resulting_labels = [tick_labels[i] for i in ticks]
+
+        # Set y limits for each row of subplots
         if ylims is None:
             axs[0, 0].set_ylim([0, .5])
             axs[1, 0].set_ylim([-10, 17])
@@ -246,6 +238,7 @@ def plot_flex_prices(power, output_path, save_figure=True, figure_path='figures/
         axs[0, 0].set_xlim([0, 97])
         axs[0, 0].set_xticks(ticks)
 
+        # Rotate x labels
         for i in range(ncols):
             axs[nrows - 1, i].set_xticklabels(resulting_labels, rotation=45)
 
@@ -258,8 +251,7 @@ def plot_flex_prices(power, output_path, save_figure=True, figure_path='figures/
 
 
 if __name__ == '__main__':
-    # plot_n_avail_veh(output_path='../output/3.7/', figure_path='../figures/')
-    # ylim_dict for testing
+    # To run without running aggregate.py: ylim_dict for testing
     ylim_dict = {'forecast': {'max': 0.36, 'min': 0.13},
                  'flex power': {'max': 12, 'min': -16},
                  'flex price': {'max': .6, 'min': -0.4}}
